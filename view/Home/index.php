13ce51dab50298c8c10d8259cdf7e0663f3e9e7a
abrahampadron@gmail.com <abrahampadron@gmail.com>
2023-03-04 20:23:05 -0600

    se cambio el inicio de sesión

--- view/Home/index.php
+++ view/Home/index.php
@@ -1,27 +1,32 @@
+<?php
+require_once("../../config/conexion.php");
+if(isset($_SESSION["usu_id "])){
+?>
 <!DOCTYPE html>
 <html>
 <?php require_once("../MainHead/head.php"); ?>
-
-<title>Registro de Contratos y Pagos: Home</title>
+	<title>Registro de Contratos y Pagos: Home</title>
 </head>
-
 <body class="with-side-menu">
-<?php require_once("../MainHeader/header.php"); ?>
+	
+	<?php require_once("../MainHeader/header.php");?>
 
 	<div class="mobile-menu-left-overlay"></div>
 
-    <?php require_once("../MainNav/nav.php"); ?>
+    <?php require_once("../MainNav/nav.php");?>
 
 	<div class="page-content">
 		<div class="container-fluid">
 			<h1>Inicio</h1>
-
-
-            
-		</div><!--.container-fluid-->
-	</div><!--.page-content-->
+		</div>
+	</div>
     
-    <?php require_once("../MainJs/js.php"); ?>
-	
+    <?php require_once("../MainJs/js.php");?>
 </body>
 </html>
+<?php
+} else {
+    header("Location:".Conectar::ruta()."index.php");
+  }
+
+?>
